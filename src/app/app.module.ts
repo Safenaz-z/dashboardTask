dec9ad15bdadf09a76981f983f61dacc648d28c9
safi.zakaria <s.zakaria@inet.works>
2020-07-09 15:02:47 +0300

    addin ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,54 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import { ToastrModule } from 'ngx-toastr';
+import { MaterialsModule } from './materials/materials.module';
+import { AddUserModalComponent } from './add-user-modal/add-user-modal.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ArabicDashboardComponent } from './arabic-dashboard/arabic-dashboard.component';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserModalComponent,
+    ArabicDashboardComponent,
+  ],
+  entryComponents:[
+    AddUserModalComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added,
+    MaterialsModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+      }
+  })
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
+ export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+import {MatDialogModule} from '@angular/material/dialog';
+
+import { EditModalComponent } from '../edit-modal/edit-modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+
+@NgModule({
+  declarations: [DashboardComponent,EditModalComponent],
+  entryComponents:[EditModalComponent],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,MatDialogModule,
+    ReactiveFormsModule,
+    FormsModule,
+    TranslateModule.forRoot()
+  ]
+})
+export class DashboardModule { }
